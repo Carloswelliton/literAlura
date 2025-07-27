2d0ccd5d050e9f33ca14c015d4169235ef27f59c
Carlos <carloswelliton7@gmail.com>
2025-07-27 13:50:05 -0400

    criando classe HttpConnection para se conectar com a API do Gutendex e verificando se é exbido no console após a implementação do CommandoLineRunner

--- src/main/java/spring_boot/literalura/LiteraluraApplication.java
+++ src/main/java/spring_boot/literalura/LiteraluraApplication.java
@@ -1,13 +1,23 @@
 package spring_boot.literalura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import spring_boot.literalura.service.HttpConection;
+
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		HttpConection api = new HttpConection();
+
+		System.out.println(api.connection("https://gutendex.com/books/"));
 	}
 
 }
---
+++ src/main/java/spring_boot/literalura/service/HttpConection.java
@@ -0,0 +1,38 @@
+package spring_boot.literalura.service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class HttpConection {
+
+  public String connection(String address){
+    HttpClient client  = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+      .uri(URI.create(address))
+      .timeout(Duration.ofSeconds(30))
+      .header("Accept","application/json")
+      .GET()
+      .build();
+
+    HttpResponse<String> response = null;
+
+    try {
+      response = client
+        .send(request, HttpResponse.BodyHandlers.ofString());
+      
+      if(response.statusCode()!=200){
+        throw new RuntimeException("erro na busca da API "+ response.statusCode() + "\nresposta: " + response.body());
+      }
+
+      return response.body();
+    
+    } catch (Exception e) {
+      throw new RuntimeException("Falha ao conectar com: " + address, e);
+    }
+    
+  }
+  
+}
